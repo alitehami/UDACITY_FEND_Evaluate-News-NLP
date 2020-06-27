ad7a33c1ac9dbc24bf84c14e52e49b9ab14f2b7f
ali-tehami <ali.tehami@yahoo.com>
2020-06-28 03:38:07 +1000

    initial project setup: -webpack configs for dev & prod -service workers -scss loaders and minifiers -required npm libs -test server-side api calls and GET requests

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,57 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express()
+const path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const owmAPIResponse = require("./owmAPI.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(express.static('dist'))
+// Require the Aylien npm package
+const aylien = require("aylien_textapi");
 
-console.log(__dirname)
+let json = {
+  title: "test json response",
+  message: "this is a message",
+  time: "now",
+};
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+const app = express();
+app.use(cors());
+// to use json
+app.use(bodyParser.json());
+// to use url encoded values
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("dist"));
+
+console.log(JSON.stringify(mockAPIResponse));
+
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
+
+app.get("/test", function (req, res) {
+  res.json(mockAPIResponse);
+});
+
+app.get("/owmMelbourne", (request, response) => {
+  owmAPIResponse().then((x) => {
+    console.log("GET owm melbourne recieved....");
+    console.log(x);
+    response.json(x);
+  });
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+console.log(`Your API KEY is ${process.env.API_KEY}`);
+console.log(`Your APP ID key is ${process.env.API_ID}`);
